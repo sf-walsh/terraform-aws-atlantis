e925678bf9ecbcf4bb1b8d252209dab46aa64250
[Sean Walsh] <[sf.walsh@gmail.com]>
2022-10-20 14:14:56 -0400

    added example for infracost

---
+++ examples/github-complete-w-infracost/data.tf
@@ -0,0 +1,63 @@
+################################################################################
+# Supporting Resources
+################################################################################
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_elb_service_account" "current" {}
+
+
+#####################
+### VPC, Availability Zones and Subnets
+#####################
+
+
+data "aws_vpc" "custom" {
+  default = false
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_subnet" "private_a" {
+  vpc_id = data.aws_vpc.custom.id
+
+  tags = {
+    Name = "*-private-a"
+  }
+}
+
+data "aws_subnet" "private_b" {
+  vpc_id = data.aws_vpc.custom.id
+
+  tags = {
+    Name = "*-private-b"
+  }
+}
+
+data "aws_subnet" "public_a" {
+  vpc_id = data.aws_vpc.custom.id
+
+  tags = {
+    Name = "*-public-a"
+  }
+}
+
+data "aws_subnet" "public_b" {
+  vpc_id = data.aws_vpc.custom.id
+
+  tags = {
+    Name = "*-public-b"
+  }
+}
+
+data "aws_acm_certificate" "atlantis" {
+  domain      = "atlantis-ecs-236211037.us-east-1.elb.amazonaws.com" #"${local.name}.${var.domain}"
+  most_recent = true
+  statuses    = ["ISSUED"]
+  types       = ["IMPORTED"]
+  key_types   = ["RSA_2048"]
+}
---
+++ examples/github-complete-w-infracost/main.tf
@@ -0,0 +1,228 @@
+locals {
+  name   = "atlantis-ecs"
+  region = "us-east-1"
+
+  tags = {
+    Owner       = "wuxt"
+    Environment = "dev"
+  }
+}
+
+##############################################################
+# Atlantis Service
+##############################################################
+
+module "atlantis" {
+  source = "../../"
+
+  name = local.name
+
+  # VPC
+  vpc_id             = data.aws_vpc.custom.id
+  azs                = ["${local.region}a", "${local.region}b"]
+  private_subnet_ids = [data.aws_subnet.private_a.id, data.aws_subnet.private_b.id]
+  public_subnet_ids  = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id]
+  internal           = false
+
+  # EFS
+  enable_ephemeral_storage = true
+
+  # ECS
+  create_ecs_cluster           = true
+  ecs_service_platform_version = "LATEST"
+  ecs_container_insights       = true
+  ecs_task_cpu                 = 512
+  ecs_task_memory              = 1024
+  container_memory_reservation = 256
+  container_cpu                = 512
+  container_memory             = 1024
+
+  runtime_platform = {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  entrypoint        = ["docker-entrypoint.sh"]
+  command           = ["server"]
+  working_directory = "/tmp"
+  docker_labels = {
+    "org.opencontainers.image.title"       = "Atlantis"
+    "org.opencontainers.image.description" = "A self-hosted golang application that listens for Terraform pull request events via webhooks."
+    "org.opencontainers.image.url"         = "https://github.com/runatlantis/atlantis/pkgs/container/atlantis"
+  }
+  start_timeout = 30
+  stop_timeout  = 30
+
+  readonly_root_filesystem = false # atlantis currently mutable access to root filesystem
+  ulimits = [{
+    name      = "nofile"
+    softLimit = 4096
+    hardLimit = 16384
+  }]
+
+  # DNS
+  route53_zone_name    = var.domain
+  route53_private_zone = true
+
+  # Trusted roles
+  trusted_entities = ["arn:aws:iam::230570994065:role/delegatedadmin/developer/atlantis-xaccount-test-role"]
+  trusted_principals   = ["ssm.amazonaws.com", "ecs-tasks.amazonaws.com"]
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/cms-cloud-admin/developer-boundary-policy"
+  path                 = "/delegatedadmin/developer/"
+
+  # Atlantis
+  atlantis_github_user       = var.github_user
+  atlantis_github_user_token = var.github_token
+  atlantis_repo_allowlist    = [for repo in var.github_repo_names : "github.com/${var.github_owner}/${repo}"]
+  infracost_api_key               = var.infracost_api_key
+  atlantis_image = var.atlantis_image
+
+  # ALB access
+  alb_ingress_cidr_blocks         = var.alb_ingress_cidr_blocks
+  alb_logging_enabled             = true
+  alb_log_bucket_name             = module.atlantis_access_log_bucket.s3_bucket_id
+  alb_log_location_prefix         = "atlantis-alb"
+  alb_listener_ssl_policy_default = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  alb_drop_invalid_header_fields  = true
+  allow_unauthenticated_access    = true
+  allow_github_webhooks           = true
+  allow_repo_config               = true
+  certificate_arn                 = data.aws_acm_certificate.atlantis.arn
+
+  tags = local.tags
+}
+
+################################################################################
+# GitHub Webhooks
+################################################################################
+
+module "github_repository_webhook" {
+  source = "../../modules/github-repository-webhook"
+  # version = "3.21.0"
+
+  github_owner = var.github_owner
+  github_token = var.github_token
+
+  atlantis_repo_allowlist = var.github_repo_names
+
+  webhook_url    = "https://atlantis-ecs-236211037.us-east-1.elb.amazonaws.com/events" #module.atlantis.atlantis_url_events
+  webhook_secret = module.atlantis.webhook_secret
+  webhook_content_type = "json"
+  webhook_insecure_ssl = true
+
+}
+
+################################################################################
+# ALB Access Log Bucket + Policy
+################################################################################
+module "atlantis_access_log_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket = "atlantis-access-logs-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.atlantis_access_log_bucket_policy.json
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  force_destroy = true
+
+  tags = local.tags
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "all"
+      enabled = true
+
+      transition = [
+        {
+          days          = 30
+          storage_class = "ONEZONE_IA"
+          }, {
+          days          = 60
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 90
+      }
+
+      noncurrent_version_expiration = {
+        days = 30
+      }
+    },
+  ]
+}
+
+data "aws_iam_policy_document" "atlantis_access_log_bucket_policy" {
+  statement {
+    sid     = "LogsLogDeliveryWrite"
+    effect  = "Allow"
+    actions = ["s3:PutObject"]
+    resources = [
+      "${module.atlantis_access_log_bucket.s3_bucket_arn}/*/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions
+        data.aws_elb_service_account.current.arn,
+      ]
+    }
+  }
+
+  statement {
+    sid     = "AWSLogDeliveryWrite"
+    effect  = "Allow"
+    actions = ["s3:PutObject"]
+    resources = [
+      "${module.atlantis_access_log_bucket.s3_bucket_arn}/*/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "delivery.logs.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control"
+      ]
+    }
+  }
+
+  statement {
+    sid     = "AWSLogDeliveryAclCheck"
+    effect  = "Allow"
+    actions = ["s3:GetBucketAcl"]
+    resources = [
+      module.atlantis_access_log_bucket.s3_bucket_arn
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "delivery.logs.amazonaws.com"
+      ]
+    }
+  }
+}
